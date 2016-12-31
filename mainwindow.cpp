8fb8e2638a183e8beb00eb702a6d6f872cab8bd0
Ivan Chevychelov <tschin@ya.ru>
2016-12-31 11:58:10 +0300

    Добавил верхнее контекстное меню

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,12 +7,39 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    qDebug()<<"Hello World!";
-    qDebug()<<"Ho-ho-ho";
+
+    QMenu * check = new QMenu("Выбор и расчет протяжки",ui->menuBar);
+
+            QAction* cylinder = check->addAction("Цилиндрическая протяжка");
+    connect(cylinder, SIGNAL(triggered(bool)), this, SLOT(TestSlot()));
+
+
+            QAction* facet = check->addAction("Гранная протяжка");
+    connect(facet, SIGNAL(triggered(bool)), this, SLOT(TestSlot()));
+
+
+            QAction* facet_combined = check->addAction("Гранная комбинированная протяжка");
+    connect(facet_combined, SIGNAL(triggered(bool)), this, SLOT(TestSlot()));
+
+
+            QAction* keywey = check->addAction("Шпоночная протяжка");
+    connect(keywey, SIGNAL(triggered(bool)), this, SLOT(TestSlot()));
+
+
+            QAction* slotted = check->addAction("Шлицевая протяжка");
+    connect(slotted, SIGNAL(triggered(bool)), this, SLOT(TestSlot()));
+    ui->menuBar->addMenu(check);
+
+
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::TestSlot()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -14,7 +14,8 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+private slots:
+    void TestSlot();
 private:
     Ui::MainWindow *ui;
 };
